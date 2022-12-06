9216314f72e0526cdbe18e3d401de10ae8c6cc6f
Mikael Ekberg <mikael@lakitu.io>
2022-12-06 14:29:39 +0100

    Add day 6 solution

---
+++ 2022/6-1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Err")
+	}
+
+	input := string(content)
+	length := len(input)
+	lastCharacterIndex := 0
+
+out:
+	for i := 0; lastCharacterIndex < length; i++ {
+		lastCharacterIndex = i + 4
+		fourCharacters := input[i:lastCharacterIndex]
+		total := 0
+
+		chars := []rune(fourCharacters)
+
+		for j := 0; j < len(chars); j++ {
+			char := string(chars[j])
+			charCount := strings.Count(fourCharacters, char)
+			total += charCount
+		}
+
+		if total == 4 {
+			fmt.Println(fourCharacters, lastCharacterIndex)
+			break out
+		}
+	}
+}
